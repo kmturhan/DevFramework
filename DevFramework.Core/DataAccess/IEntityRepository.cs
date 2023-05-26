9c2333627fdc7f83c08243b634a0d55160cb4fb5
kmturhan <kmturhan@gmail.com>
2023-05-26 17:20:09 +0300

    nhibernate implementation added

--- DevFramework.Core/DataAccess/IEntityRepository.cs
+++ DevFramework.Core/DataAccess/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevFramework.Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
@@ -7,8 +8,12 @@ using System.Threading.Tasks;
 
 namespace DevFramework.Core.DataAccess
 {
-	public interface IEntityRepositorycs<T>
+	public interface IEntityRepository<T> where T : class, IEntity, new() 
 	{
 		List<T> GetList(Expression<Func<T, bool>> filter = null);
+		T Get(Expression<Func<T, bool>> filter);
+		T Add(T entity);
+		T Update(T entity);
+		void Delete(T entity);
 	}
 }
---
+++ DevFramework.Core/DataAccess/NHibernate/NhQueryableRepository.cs
@@ -0,0 +1,37 @@
+﻿using DevFramework.Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+	public class NhQueryableRepository<T> : IQueryableRepository<T> where T:class, IEntity,new()
+	{
+		private NhibernateHelper _nHibernateHelper;
+		private IQueryable<T> _entities;
+        public NhQueryableRepository(NhibernateHelper nhibernateHelper)
+        {
+            _nHibernateHelper = nhibernateHelper;
+        }
+
+		public IQueryable<T> Table
+		{
+			get { return this.Entities; }
+		}
+		public virtual IQueryable<T> Entities
+		{
+			get
+			{
+				if(_entities == null)
+				{
+					_entities = _nHibernateHelper.OpenSession().Query<T>();
+				}
+				return _entities;
+			}
+		}
+	}
+}
