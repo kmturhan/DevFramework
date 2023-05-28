9138737111f810a4f0b17b3fdb5b50bd49ad926c
kmturhan <kmturhan@gmail.com>
2023-05-28 23:07:20 +0300

    mapping implementation added

---
+++ DevFramework.DataAccess.Tests/EntityFrameworkTests/EntityFrameworkTest.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.DataAccess.Tests.EntityFrameworkTests
+{
+	[TestClass]
+	public class EntityFrameworkTest
+	{
+		[TestMethod]
+		public void Get_all_returns_all_products()
+		{
+			EfProductDal productDal = new EfProductDal();
+			var result = productDal.GetList();
+
+			Assert.AreEqual(79, result.Count);
+
+
+		}
+	}
+}
